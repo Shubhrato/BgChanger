b4ddbfd66b99620474a1a8b096c11a06302c4013
Shubhrato <shubhrato18@gmail.com>
2024-10-08 13:59:16 +0530

    color update

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ function App() {
     <div className='w-screen h-screen' style={{backgroundColor:color}}>
       <div className='fixed flex flex-wrap justify-center bottom-10 inset-x-0 px-2'>
         <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white rounded-2xl px-3 py-2'>
-          <button className=' rounded-full px-4 py-1 text-white shadow-lg hover:cursor-pointer outline-none' style={{backgroundColor:"#930000"}} onClick={() => setColor("#930000")}>Red</button>
-          <button className=' rounded-full px-4 py-1 text-white shadow-lg hover:cursor-pointer outline-none' style={{backgroundColor:"#067e00"}} onClick={() => setColor("#067e00")}>Green</button>
-          <button className=' rounded-full px-4 py-1 text-white shadow-lg hover:cursor-pointer outline-none' style={{backgroundColor:"Black"}} onClick={() => setColor("Black")}>Black</button>
-          <button className=' rounded-full px-4 py-1 text-white shadow-lg hover:cursor-pointer outline-none' style={{backgroundColor:"#ff7c00"}} onClick={() => setColor("#ff7c00")}>Orange</button>
-          <button className=' rounded-full px-4 py-1 text-white shadow-lg hover:cursor-pointer outline-none' style={{backgroundColor:"White"}} onClick={() => setColor("white")}>White</button>
+          <button className=' rounded-full px-4 py-2 text-white shadow-lg hover:cursor-pointer outline-none' style={{backgroundColor:"#e5eaf5"}} onClick={() => setColor("#e5eaf5")}></button>
+          <button className=' rounded-full px-4 py-2 text-white shadow-lg hover:cursor-pointer outline-none' style={{backgroundColor:"#a28089"}} onClick={() => setColor("#a28089")}></button>
+          <button className=' rounded-full px-4 py-2 text-white shadow-lg hover:cursor-pointer outline-none' style={{backgroundColor:"#1561ad"}} onClick={() => setColor("#1561ad")}></button>
+          <button className=' rounded-full px-4 py-2 text-white shadow-lg hover:cursor-pointer outline-none' style={{backgroundColor:"#c7af6b"}} onClick={() => setColor("#c7af6b")}></button>
+          <button className=' rounded-full px-4 py-2 text-black shadow-lg hover:cursor-pointer outline-none' style={{backgroundColor:"#94f0f1"}} onClick={() => setColor("#94f0f1")}></button>
         </div>
       </div>
     </div>
